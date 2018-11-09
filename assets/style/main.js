ed9ea6aac71d45a52b865f2e20f01ede08479cc2
jubyshu <jt.lowah@gmail.com>
2018-11-09 20:00:02 +0800

    daily change

--- assets/style/main.js
+++ assets/style/main.js
@@ -36,18 +36,18 @@ $(document).ready(function() {
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
 
-  $('.navigation-wrapper .blog-button').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
+  // $('.navigation-wrapper .blog-button').click(function() {
+  //   if ($('.navigation-wrapper').css('display') == "block") {
+  //     $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+  //       $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+  //       $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+  //     });
 
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-    }
+  //     $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+  //   }
     
-    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
-  });
+  //   $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+  // });
 });
 
 // back to top
