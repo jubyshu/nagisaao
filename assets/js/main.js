4fcf9c64ae1219efa80ec39275c56bb7e0572396
jubyshu <jt.lowah@gmail.com>
2019-04-12 17:14:25 +0800

    update post

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,22 +16,22 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	var dateBegin = new Date("2015/01/03 23:15:15");
-	var dateEnd = new Date();
-	var dateDiff = dateEnd.getTime() - dateBegin.getTime();
-	var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));
-	var timeLeft = dateDiff % (24 * 3600 * 1000);
-	var hours = Math.floor(timeLeft / (3600 * 1000));
-	var time = dayDiff + "d " + hours + "h";
+	let dateBegin = new Date("2015/01/03 23:15:15");
+	let dateEnd = new Date();
+	let dateDiff = dateEnd.getTime() - dateBegin.getTime();
+	let dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));
+	let timeLeft = dateDiff % (24 * 3600 * 1000);
+	let hours = Math.floor(timeLeft / (3600 * 1000));
+	let time = dayDiff + "d " + hours + "h";
 	$("#runtime").html(time);
 });
 
-var toc = document.querySelector(".post-toc");
+let toc = document.querySelector(".post-toc");
 function tocShow() {
-  var clientHeight = document.documentElement.clientHeight;	
-	var clientWidth = document.documentElement.clientWidth;
-	var tocWrapper = document.querySelector(".content-wrapper__inner");
-  var leftMargin = (clientWidth - tocWrapper.clientWidth) / 2 - toc.clientWidth - 50;
+  let clientHeight = document.documentElement.clientHeight;	
+	let clientWidth = document.documentElement.clientWidth;
+	let tocWrapper = document.querySelector(".content-wrapper__inner");
+  let leftMargin = (clientWidth - tocWrapper.clientWidth) / 2 - toc.clientWidth - 50;
 	if (toc.clientHeight < clientHeight * 0.6 && leftMargin >= 50) {
 		toc.style.visibility = "visible";
 	} else {
@@ -44,11 +44,11 @@ if ( !! toc) {
 }
 
 function tocScroll(){
- var alis = $('.post :header');
- var toc_alis = $('.post-toc').find('a');
- var scroll_height = $(window).scrollTop();
- for(var i =0; i<alis.length; i++){
-   var a_height = $(alis[i]).offset().top;
+ let alis = $('.post :header');
+ let toc_alis = $('.post-toc').find('a');
+ let scroll_height = $(window).scrollTop();
+ for(let i =0; i<alis.length; i++){
+   let a_height = $(alis[i]).offset().top;
    if (a_height < scroll_height){
      toc_alis.removeClass('active');
      $(toc_alis[i]).addClass('active');
@@ -58,3 +58,10 @@ function tocScroll(){
 $(function() {
  $(window).bind('scroll',tocScroll); 
 });
+
+let imgArr = document.querySelectorAll('.post-img');
+for (let i = 0; i < imgArr.length; i++) {
+  imgArr[i].onclick = function() {
+    window.open(imgArr[i].src, '_self');
+  };
+}
